534f33577983573757e428f4bdfc818f57a123ca
Lindsey <lindsey@koti.tech>
2026-02-06 21:29:21 +0000

    Fix SSL: always accept DO managed DB self-signed cert

--- server/db/pool.js
+++ server/db/pool.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
